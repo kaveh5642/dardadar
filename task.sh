ee25a6c12ef2e6c80629bdbd159b745b5f6d7ee4
kaveh5642 <kaveh5642@hotmail.com>
2026-05-03 10:45:42 +0000

    Result: 20260503_104542 [skip ci]

--- task.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pkill -9 gost
-
-# اتصال از طریق WebSocket Secure (wss) به پورت 443 کداسپیس
-# جایگزین کردن آدرس کداسپیس خودت الزامی است
-nohup ./gost -L rtcp://:2080/127.0.0.1:2080 -F relay+wss://upgraded-halibut-x5g499jx75gjf9xrp-8080.app.github.dev:443?host=raw.githubusercontent.com > gost.log 2>&1 &
-
-echo "Tunnel attempt started..."
