752ba5828db9bcd4fca30d1f51f70460792c91f9
kaveh5642 <kaveh5642@hotmail.com>
2026-05-03 10:45:36 +0000

    update tunnel settings

--- task.sh
+++ task.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 pkill -9 gost
 
-# استفاده از wss برای عبور از پروکسیِ خودِ گیت‌هاب
+# اتصال از طریق WebSocket Secure (wss) به پورت 443 کداسپیس
+# جایگزین کردن آدرس کداسپیس خودت الزامی است
 nohup ./gost -L rtcp://:2080/127.0.0.1:2080 -F relay+wss://upgraded-halibut-x5g499jx75gjf9xrp-8080.app.github.dev:443?host=raw.githubusercontent.com > gost.log 2>&1 &
 
-echo "Gost WSS Tunnel started..."
+echo "Tunnel attempt started..."
