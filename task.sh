e2ac20787405d167619756b7e0382d0d0a5e522b
kaveh5642 <kaveh5642@hotmail.com>
2026-05-03 10:41:04 +0000

    update tunnel settings

--- task.sh
+++ task.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-
-# ۱. دانلود Gost در سرور ایران
-if [ ! -f "./gost" ]; then
-    curl -sL https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost_2.11.1_linux_amd64.tar.gz | tar xz
-    mv gost_2.11.1_linux_amd64/gost .
-    chmod +x gost
-fi
-
-# ۲. کشتن پروسه‌های قبلی
+# کشتن پروسه‌های قبلی
 pkill -9 gost
 
-# ۳. اجرای در پس‌زمینه (اضافه شدن nohup و &)
-nohup ./gost -L rtcp://:2080/127.0.0.1:2080 -F relay+wss://upgraded-halibut-x5g499jx75gjf9xrp-8080.app.github.dev:8080?host=raw.githubusercontent.com > /dev/null 2>&1 &
+# اجرای تانل با پروتکل TLS (امن‌تر و پایدارتر)
+# پارامتر secure=false برای این است که به گواهی SSL کداسپیس گیر ندهد
+nohup ./gost -L rtcp://:2080/127.0.0.1:2080 -F relay+tls://upgraded-halibut-x5g499jx75gjf9xrp-8080.app.github.dev:8080?secure=false > gost.log 2>&1 &
 
-echo "Gost is running in background..."
+echo "Gost TLS started..."
